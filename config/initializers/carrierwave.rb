72809b4c3e915a6e47674449ee5960fdfeeddf71
Paul Yun <paul.a.yun@gmail.com>
2015-07-16 15:02:00 -0400

    Fix Hound Violations

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,13 @@
 CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],     # required
+    provider: 'AWS', # required
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], # required
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], # required
-    # region:                ENV["AWS_REGION"],            # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    # region: ENV["AWS_REGION"], # optional, defaults to 'us-east-1'
+    # host: 's3.example.com', # optional, defaults to nil
+    # endpoint: 'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = ENV["S3_BUCKET"]                             # required
-  # config.fog_public     = false                                        # optional, defaults to true
-  # config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+  config.fog_directory  = ENV["S3_BUCKET"] # required
 
   # config.storage    = :aws
   # config.aws_bucket = ENV.fetch('S3_BUCKET')
