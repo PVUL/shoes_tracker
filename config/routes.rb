aa90298c53cf385217c38c74ca4443d6c3633d97
Paul Yun <paul.a.yun@gmail.com>
2015-07-19 21:27:46 -0400

    Add new background image. Create welcome index template. Create Welcome controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  root 'shoes#index'
+  # root 'shoes#index'
   devise_for :users
+  get 'welcome/index'
   resources :shoes, only: [:index, :shoe, :destroy] do
     resources :check_ins
   end
   resources :user_shoes, only: [:new, :create]
+
+  authenticated :user do
+    root 'shoes#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
 end
