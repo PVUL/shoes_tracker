dcf3d8d4f1b9ea06c571d6fc2a4862909f7b775b
Paul Yun <paul.a.yun@gmail.com>
2015-07-22 01:07:45 -0400

    Add Shoe of week feature. Add Last Check in Feature

--- app/controllers/shoes_controller.rb
+++ app/controllers/shoes_controller.rb
@@ -1,9 +1,15 @@
 class ShoesController < ApplicationController
+  include ActionView::Helpers::DateHelper
+
   def index
     if !current_user.nil?
       @user = current_user
       @user_shoes = UserShoe.where(user: @user)
       @weekly_check_ins = CheckIn.where(created_at: Date.today.at_beginning_of_week..Time.now)
+      @last_check_in = CheckIn.where(user_shoe: @user_shoes).last.created_at
+      @time_since_last_check_in = time_ago_in_words(@last_check_in, include_seconds: true).concat(" ago")
+      @trending_user_shoe = @weekly_check_ins.group('user_shoe_id').order('count(*) desc').limit(1).pluck('user_shoe_id').first
+      @shoe_of_week = UserShoe.find(@trending_user_shoe)
     else
       @user_shoes = UserShoe.all
     end
